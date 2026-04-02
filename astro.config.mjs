684c2bc8e778c8ed47f692b1218e7ba304355189
Gene-Xia-Jining <gene.xia@icloud.com>
2026-04-02 16:49:15 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,5 +23,6 @@ export default defineConfig({
 				},
 			],
 		}),
+		mdx(),
 	],
 });
